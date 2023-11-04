24877617006dc4319428d97b0f2e84265c58e5b3
gcc_bug <by.gdc@outlook.com>
2023-11-04 22:47:18 +0800

    need complex value and tensors convert

--- main.cpp
+++ main.cpp
@@ -1,81 +1,10 @@
 #pragma once
 #include <iostream>
-#include <dd/Tensor.hpp>
-#include <dd/Export.hpp>
-dd::ComplexValue one = { 1,0 };
-dd::ComplexValue zero = { 0,0 };
-void tensor_hyper_index() {
-    auto dd1 = std::make_unique<dd::Package<>>(10);
-    dd1->varOrder = { {"x0",0},{"y0",1},{"x1",2},{"y1",3} };
 
-    xt::xarray<dd::ComplexValue> U = {
-        {{{one, zero}, {zero, one}}, {{zero, zero}, {zero, zero}}},
-        {{{zero, zero}, {zero, zero}}, {{zero, one}, {one, zero}}}
-    };
-    std::vector<dd::Index> indexs = { {"x0",0},{"x0",1},{"x1",0},{"y1",0}, };
-
-    dd::Tensor tn = { U,indexs, "cx" };
-    dd::TDD tdd = tn.to_tdd(dd1);
-    std::cout << tdd.e.p->v << std::endl;
-    dd::export2Dot(tdd.e, "cnot_hyber");
-}
-void tensor_2_tdd_cx_4() {
-    auto dd1 = std::make_unique<dd::Package<>>(10);
-    dd1->varOrder = { {"x0",0},{"y0",1},{"x1",2},{"y1",3} };
-    xt::xarray<dd::ComplexValue> U = {
-        {one, zero, zero, zero},
-        {zero, one, zero, zero},
-        {zero, zero, zero, one},
-        {zero, zero, one, zero}
-    };
-
-    std::vector<dd::Index> indexs = { {"y2",3},{"x0",1} };
-    // dd::Tensor tn = {U,indexs, "cx"};
-    dd::Tensor tn(U, indexs);
-    dd::TDD tdd = tn.to_tdd(dd1);
-    std::cout << tdd.e.p->v << std::endl;
-    dd::export2Dot(tdd.e, "cnot_4_4");
-}
-
-void tensor_z() {
-    auto dd1 = std::make_unique<dd::Package<>>(10);
-    dd1->varOrder = { {"x0",0},{"y0",1},{"x1",5},{"y1",6} };
-
-    xt::xarray<dd::ComplexValue> U = {
-        {one, zero},
-        {zero, {-1,0}},
-    };
-    std::vector<dd::Index> indexs = { {"x1",2},{"x1",2}};
-
-    dd::Tensor tn = { U,indexs, "z" };
-    dd::TDD tdd = tn.to_tdd(dd1);
-    std::cout << tdd.e.p->v << std::endl;
-    dd::export2Dot(tdd.e, "cnot_z");
-}
-void tensor_2_tdd_cx_2() {
-    auto dd1 = std::make_unique<dd::Package<>>(10);
-    dd1->varOrder = { {"x0",0},{"y0",1},{"x1",5},{"y1",6} };
-
-    xt::xarray<dd::ComplexValue> U = {
-        {{{one, zero}, {zero, one}}, {{zero, zero}, {zero, zero}}},
-        {{{zero, zero}, {zero, zero}}, {{zero, one}, {one, zero}}}
-    };
-    std::vector<dd::Index> indexs = { {"x1",2},{"y1",3}, {"x0",0},{"y0",1} };
-
-    dd::Tensor tn = { U,indexs, "cx" };
-    dd::TDD tdd = tn.to_tdd(dd1);
-    std::cout << tdd.e.p->v << std::endl;
-    dd::export2Dot(tdd.e, "cnot_2_2_2_2");
-}
 int main(int argc, char *argv[]) {
-    std::cout << "cx _2" << std::endl;
-    tensor_2_tdd_cx_2();
-    std::cout << "cx _4" << std::endl;
-    tensor_2_tdd_cx_4();
-    std::cout << "hyber" << std::endl;
-    tensor_hyper_index();
-
-    std::cout << "z gate" << std::endl;
-    tensor_z();
+    std::cout << "hello" << std::endl;
+    for (int i = 0; i < argc; ++i) {
+        std::cout << argv[i] << std::endl;
+    }
     return 0;
 }
