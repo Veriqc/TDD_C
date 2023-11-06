1d37763e89fdd3a3e19719439227c8f68e7b6dca
gcc_bug <by.gdc@outlook.com>
2023-11-06 11:49:21 +0800

    add index_set read

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <regex>
 
 #include <dd/Tensor.hpp>
+#include <dd/Tdd.hpp>
 #include <dd/ComplexValue.hpp>
 
 #include <xtensor/xarray.hpp>
@@ -14,8 +15,6 @@
 #include <xtensor/xadapt.hpp>
 
 dd::ComplexValue string_2complex(const std::string s){
-    std::cout << "---------------" << std::endl;
-    std::cout << "s:" << s << std::endl;
     std::regex pattern(R"((-?\d+\.\d+)([+-])(\d+\.\d+))");
     std::smatch matches;
 
@@ -26,12 +25,10 @@ dd::ComplexValue string_2complex(const std::string s){
         if (sign == '-') {
                 imag = -imag;
             }
-        std::cout << "real: " << real << std::endl;
-        std::cout << "imag: " << imag << std::endl;
         return dd::ComplexValue(real,imag);
     }
     else{
-        std::cout << "faile" << std::endl;
+        std::cerr << "faile" << std::endl;
     }
 }
 
@@ -49,30 +46,28 @@ int main(int argc, char *argv[]) {
     }
 
     std::string line;
+    std::getline(inputFile, line);
     while (std::getline(inputFile, line)) {
         std::vector<dd::ComplexValue> data;
         std::vector<int> shape;
+        std::vector<dd::Index> index_set;
         std::string temp;
-        std::cout << line << std::endl;
 
         std::istringstream ss(line);
         std::string token;
 
         std::getline(ss, token, ',');
-        std::cout << token << std::endl;
         std::istringstream datass(token);
         while( std::getline(datass, temp, ' ')){
-            if(temp.empty()){ continue;}
-            std::cout << string_2complex(temp) << std::endl;
+            if(temp.compare(" ")==0 or temp.empty()){ continue;}
             data.push_back(string_2complex(temp));
         }
          
 
         std::getline(ss, token, ',');
-        std::cout << token << std::endl;
         std::istringstream shapess(token);
         while( std::getline(shapess, temp, ' ')){
-            if(temp.empty()){ continue;}
+            if(temp.compare(" ")==0 or temp.empty()){ continue;}
             try {
                 int value = std::stoi(token);
                 shape.push_back(value);
@@ -80,11 +75,42 @@ int main(int argc, char *argv[]) {
                 std::cerr << "Invalid input format." << std::endl;
                 return 1;
             }
-            std::cout << temp << std::endl;
         }
 
         xt::xarray<dd::ComplexValue> res = xt::adapt(data,shape);
         std::cout << res  << std::endl;
+
+        std::getline(ss, token, ',');
+        std::istringstream index_setss(token);
+        while(std::getline(index_setss, temp, ' ')){
+            if(temp.compare(" ")==0 or temp.empty()){ continue;}
+
+            std::istringstream index_partss(temp);
+            std::vector<std::string> parts;
+            std::string part;
+            while (std::getline(index_partss, part, ':')) {
+                parts.push_back(part);
+            }
+            if (parts.size() == 2) {
+                std::string firstPart = parts[0];
+                int secondPart;
+                
+                // Convert the second part to an integer
+                try {
+                    secondPart = std::stoi(parts[1]);
+                    index_set.push_back(dd::Index(firstPart,secondPart));
+                } catch (const std::invalid_argument& e) {
+                    std::cerr << "Invalid integer in the second part: " << e.what() << std::endl;
+                    return 1;
+                }
+            } else {
+                continue;
+            }
+        }
+        for(auto index_temp: index_set){
+            std::cout << "index key: " << index_temp.key << " index idx: " << index_temp.idx << std::endl;
+        }
+        dd::Tensor tn = dd::Tensor(res,index_set);
     }
 
     inputFile.close();
