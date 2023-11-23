21ef2184b9478b4fd96f885d11c586712b905fe6
gcc_bug <by.gdc@outlook.com>
2023-11-23 19:26:45 +0800

    modify folder

--- include/python-c.cpp
+++ include/python-c.cpp
@@ -1,5 +1,6 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <complex>
 #include "dd/Tdd.hpp"
 #include "dd/Tensor.hpp"
 #include "Cir_tn.hpp"
@@ -8,13 +9,13 @@
 #include <xtensor-python/pyarray.hpp>
 class xarrayClass {
     private:
-    xt::xarray<double> data;
-    std::vector<xt::xarray<double>> datas;
+    xt::xarray<std::complex<double>> data;
+    std::vector<xt::xarray<std::complex<double>>> datas;
     public:
-        xarrayClass(const xt::xarray<double>& data_,const std::vector<xt::xarray<double>>& datas_ = {}):
+        xarrayClass(const xt::xarray<std::complex<double>>& data_,const std::vector<xt::xarray<std::complex<double>>>& datas_ = {}):
         data(data_), datas(datas_){}
 
-        void add(xt::xarray<double> array){
+        void add(xt::xarray<std::complex<double>> array){
             this->datas.push_back(array);
         }
         void infor(){
@@ -35,7 +36,7 @@ int add(int i, int j) {
 namespace py = pybind11;
 void BindArray(py::module& m){
     py::class_<xarrayClass>(m, "xarrayClass")
-        .def(py::init<const xt::xarray<double>&, const std::vector<xt::xarray<double>>&>())
+        .def(py::init<const xt::xarray<std::complex<double>>&, const std::vector<xt::xarray<std::complex<double>>>&>())
         .def("add", &xarrayClass::add)
         .def("infor", &xarrayClass::infor);
 
@@ -51,17 +52,18 @@ void BindComplex(py::module& m){
 
 void BindIndex(py::module& m){
     py::class_<dd::Index>(m, "Index")
-        .def(py::init<>())  // Default constructor
-        .def(py::init<const std::string, const short>())  // Constructor with parameters
-        .def_readwrite("key", &dd::Index::key)  // Expose member r
-        .def_readwrite("idx", &dd::Index::idx); // Expose member i
+        .def(py::init<>()) 
+        .def(py::init<const std::string, const short>())  
+        .def_readwrite("key", &dd::Index::key) 
+        .def_readwrite("idx", &dd::Index::idx); 
 }
 PYBIND11_MODULE(TDD, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
     m.def("add", &add, "A function which adds two numbers");
 
-    BindArray(m);
+    
     BindComplex(m);
     BindIndex(m);
+    BindArray(m);
 }
