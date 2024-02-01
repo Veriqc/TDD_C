4411db15c1534dab56efa54da63b02633fb65f47
gcc-bug <by.gdc@outlook.com>
2024-02-01 23:17:44 +0800

    rewrite makezerostate

--- test/test_zero.cpp
+++ test/test_zero.cpp
@@ -15,9 +15,15 @@ using namespace dd;
 int main() {
     auto dd1 = std::make_unique<dd::Package<>>(10);
     dd1->varOrder = { {"x0",0},{"y0",1},{"x1",2},{"y1",3} };
+    dd::ComplexValue one = { 1,0 };
+    dd::ComplexValue zero = { 0,0 };
+    xt::xarray<dd::ComplexValue> U = {one,zero};
+    std::vector<dd::Index> indexs = {{"x0",0} };
 
-    auto e = dd1->makeZeroState(4);
-    dd::export2Dot(e, "zero");
+    dd::Tensor tn = { U,indexs, "zero" };
+    dd::TDD tdd = tn.to_tdd(dd1.get());
+    std::cout << tdd.e.p->v << std::endl;
+    dd::export2Dot(tdd.e, "zero");
     return 0;
 }
 
